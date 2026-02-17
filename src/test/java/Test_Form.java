7bab056ad4a23a5e955c6c53bd8ea2f03895e2aa
Oleg Razumov <orazumoff@gmail.com>
2026-02-17 14:11:57 +0300

    second commit

---
+++ src/test/java/Test_Form.java
@@ -0,0 +1,21 @@
+package tests;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+
+
+public class Test_Form {
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillFormTest() {
+       
+    }
+
+}
