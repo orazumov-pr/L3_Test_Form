adf511b4a1571358dcebcba7a60c456ffa75ef3e
Oleg Razumov <orazumoff@gmail.com>
2026-02-22 20:32:25 +0300

    add namePicture value

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -11,6 +11,7 @@ public class TestData {
     public static String daySelect ="14";
     public static String subjectsInput ="En";
     public static String hobbieInput ="Music";
+    public static String namePicture ="my_abstract_scr.jpg";
     public static String address ="Ulitsa Lenina, 5";
     public static String state ="Uttar Pradesh";
     public static String city ="Merrut";
